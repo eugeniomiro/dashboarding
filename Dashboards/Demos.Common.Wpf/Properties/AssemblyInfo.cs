a4351eb52fc7ea5e0755a98e3b37e31895a35614
SND\david_black_uk_cp <SND\david_black_uk_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2009-07-04 13:22:18 +0000

    final changes
    git-svn-id: https://dashboarding.svn.codeplex.com/svn@26483 81fbe566-5dc4-48c1-bdea-7421811ca204

--- Dashboards/Demos.Common.Wpf/Properties/AssemblyInfo.cs
+++ Dashboards/Demos.Common.Wpf/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Demos.Common.Wpf")]
+[assembly: AssemblyTitle("Demos.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Demos.Common.Wpf")]
+[assembly: AssemblyProduct("Demos.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
